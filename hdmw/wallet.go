d33dadf656a8f15eb0aaaf0b61510a97b283ade4
orpheus <ryanachacon@gmail.com>
2018-08-19 22:14:26 -0700

    created minimal structures for child nodes

--- hdmw/wallet.go
+++ hdmw/wallet.go
@@ -1,28 +1,77 @@
 package hdmw
 
 import (
-	"github.com/tyler-smith/go-bip39"
-	"github.com/btcsuite/btcutil/hdkeychain"
+	"database/sql/driver"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/tyler-smith/go-bip39"
 )
 
-type Wallet struct {
-	Mnemonic          string
-	Seed              []byte
-	Entropy           []byte
-	MasterExtendedKey *hdkeychain.ExtendedKey
+type AddressNode struct {
+	ExtendedKey *hdkeychain.ExtendedKey
+	//@ToDo: Fill out struct
 }
 
-func CreateWallet(password string) *Wallet {
+type ChainNode struct {
+	ExtendedKey *hdkeychain.ExtendedKey
+	isExternal  bool
+	isInternal  bool
+	Addresses   []*AddressNode
+}
+
+type AccountNode struct {
+	ExtendedKey   *hdkeychain.ExtendedKey
+	ExternalChain []*ChainNode
+	InternalChain []*ChainNode
+	isHardened    bool
+}
+
+type CoinNode struct {
+	ExtendedKey *hdkeychain.ExtendedKey
+	Name        string
+	Ticker      string
+	//@ToDo: Add Network struct
+	Network    driver.Null
+	Accounts   []*AccountNode
+	isHardened bool
+}
+
+type PurposeNode struct {
+	ExtendedKey *hdkeychain.ExtendedKey
+	Index       uint32
+	Coins       []*CoinNode
+	isHardened  bool
+}
+
+type MasterNode struct {
+	MasterExtendedKey *hdkeychain.ExtendedKey
+	Child             *PurposeNode
+}
+
+type Wallet struct {
+	Mnemonic   string
+	Seed       []byte
+	Entropy    []byte
+	MasterNode *MasterNode
+}
+
+func CreateWalletWithPassword(password string) *Wallet {
 	entropy, _ := bip39.NewEntropy(128)
 	mnemonic, _ := bip39.NewMnemonic(entropy)
 	seed, _ := bip39.NewSeedWithErrorChecking(mnemonic, password)
+	//@ToDo: create network params for FLO and LTC, etc
 	masterKey, _ := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
-	wallet := Wallet{
-		Mnemonic:          mnemonic,
-		Seed:              seed,
-		Entropy:           entropy,
+
+	masterNode := &MasterNode{
 		MasterExtendedKey: masterKey,
 	}
-	return &wallet
+
+	wallet := &Wallet{
+		Mnemonic:   mnemonic,
+		Seed:       seed,
+		Entropy:    entropy,
+		MasterNode: masterNode,
+	}
+
+	return wallet
 }
