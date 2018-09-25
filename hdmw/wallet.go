7de74c09fcf18ec9cf8b42ba1093880954e3b621
orpheus <ryanachacon@gmail.com>
2018-09-24 20:29:43 -0700

    generate 44' purpose node

--- hdmw/wallet.go
+++ hdmw/wallet.go
@@ -1,77 +1,41 @@
 package hdmw
 
 import (
-	"database/sql/driver"
+	"fmt"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil/hdkeychain"
 	"github.com/tyler-smith/go-bip39"
 )
 
-type AddressNode struct {
-	ExtendedKey *hdkeychain.ExtendedKey
-	//@ToDo: Fill out struct
-}
-
-type ChainNode struct {
-	ExtendedKey *hdkeychain.ExtendedKey
-	isExternal  bool
-	isInternal  bool
-	Addresses   []*AddressNode
-}
-
-type AccountNode struct {
-	ExtendedKey   *hdkeychain.ExtendedKey
-	ExternalChain []*ChainNode
-	InternalChain []*ChainNode
-	isHardened    bool
-}
-
-type CoinNode struct {
-	ExtendedKey *hdkeychain.ExtendedKey
-	Name        string
-	Ticker      string
-	//@ToDo: Add Network struct
-	Network    driver.Null
-	Accounts   []*AccountNode
-	isHardened bool
-}
-
-type PurposeNode struct {
-	ExtendedKey *hdkeychain.ExtendedKey
-	Index       uint32
-	Coins       []*CoinNode
-	isHardened  bool
-}
-
-type MasterNode struct {
-	MasterExtendedKey *hdkeychain.ExtendedKey
-	Child             *PurposeNode
-}
-
 type Wallet struct {
 	Mnemonic   string
 	Seed       []byte
 	Entropy    []byte
-	MasterNode *MasterNode
+	MasterNode *hdkeychain.ExtendedKey
 }
 
 func CreateWalletWithPassword(password string) *Wallet {
-	entropy, _ := bip39.NewEntropy(128)
+	entropy, _ := bip39.NewEntropy(256)
 	mnemonic, _ := bip39.NewMnemonic(entropy)
 	seed, _ := bip39.NewSeedWithErrorChecking(mnemonic, password)
 	//@ToDo: create network params for FLO and LTC, etc
 	masterKey, _ := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
 
-	masterNode := &MasterNode{
-		MasterExtendedKey: masterKey,
-	}
-
-	wallet := &Wallet{
+	wallet := Wallet{
 		Mnemonic:   mnemonic,
 		Seed:       seed,
 		Entropy:    entropy,
-		MasterNode: masterNode,
+		MasterNode: masterKey,
 	}
 
-	return wallet
+	return &wallet
+}
+
+func (w *Wallet) GeneratePurposeNode() *hdkeychain.ExtendedKey {
+	p, err := w.MasterNode.Child(hdkeychain.HardenedKeyStart + 44)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return p
 }
--- test/hdmw/wallet_test.go
+++ test/hdmw/wallet_test.go
@@ -1,13 +1,21 @@
 package test
 
 import (
-	"testing"
-	"github.com/orpheus/bip44-hdmw/hdmw"
 	"github.com/davecgh/go-spew/spew"
-	)
+	"github.com/orpheus/bip44-hdmw/hdmw"
+	"testing"
+)
 
 func TestCreateWallet(t *testing.T) {
-	wallet := hdmw.CreateWallet("")
+	wallet := hdmw.CreateWalletWithPassword("")
 	str := spew.Sdump(wallet)
 	t.Log(str)
+}
+
+func TestCreatePurposeNode(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	str := spew.Sdump(wallet)
+	t.Log(str)
+	p := wallet.GeneratePurposeNode()
+	t.Log(p)
 }
