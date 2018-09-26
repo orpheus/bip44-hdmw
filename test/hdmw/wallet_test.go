708c788a3c899e8b68b0fb1b0c24bf3287433c49
orpheus <ryanachacon@gmail.com>
2018-09-25 17:07:36 -0700

    test: create bitcoin node

--- test/hdmw/wallet_test.go
+++ test/hdmw/wallet_test.go
@@ -16,6 +16,20 @@ func TestCreatePurposeNode(t *testing.T) {
 	wallet := hdmw.CreateWalletWithPassword("")
 	str := spew.Sdump(wallet)
 	t.Log(str)
-	p := wallet.GeneratePurposeNode()
+	p, err := wallet.GeneratePurposeNode()
+	if err != nil {
+		t.Log(err)
+	}
 	t.Log(p)
+}
+
+func TestCreateBitcoinNode(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	str := spew.Sdump(wallet)
+	t.Log(str)
+	c, err := wallet.GenerateCoinNode(hdmw.TypeBitcoin)
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(c)
 }
