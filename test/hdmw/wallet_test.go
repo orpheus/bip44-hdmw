441e012dab8543889b1db5c8cf93d0da2efd9745
orpheus <ryanachacon@gmail.com>
2018-09-27 00:30:57 -0700

    tests to derive 0 index down to addr

--- test/hdmw/wallet_test.go
+++ test/hdmw/wallet_test.go
@@ -23,3 +23,54 @@ func TestCreateAndInitWallet(t *testing.T) {
 	w := spew.Sdump(wallet)
 	t.Log(w)
 }
+
+func TestDeriveBTCAccount0(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	wallet.Initialize([]uint32{hdmw.TypeBitcoin})
+	acc, err := wallet.Coins[0].GenerateAccountNode(0)
+	if err != nil {
+		t.Error("Failed to generate account node")
+	}
+
+	a := spew.Sdump(acc)
+	t.Log(a)
+}
+
+func TestDeriveBTCChain0(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	wallet.Initialize([]uint32{hdmw.TypeBitcoin})
+	acc, err := wallet.Coins[0].GenerateAccountNode(0)
+	if err != nil {
+		t.Error("Failed to generate account node")
+	}
+
+	ch, err := acc.GenerateChainNode(0)
+	if err != nil {
+		t.Error("Failed to generate chain node")
+	}
+
+	c := spew.Sdump(ch)
+	t.Log(c)
+}
+
+func TestDeriveBTCAddress0(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	wallet.Initialize([]uint32{hdmw.TypeBitcoin})
+	acc, err := wallet.Coins[0].GenerateAccountNode(0)
+	if err != nil {
+		t.Error("Failed to generate account node")
+	}
+
+	ch, err := acc.GenerateChainNode(0)
+	if err != nil {
+		t.Error("Failed to generate chain node")
+	}
+
+	addr, err := ch.GenerateAddressNode(0)
+	if err != nil {
+		t.Error("Failed to generate address node")
+	}
+
+	a := spew.Sdump(addr)
+	t.Log(a)
+}
