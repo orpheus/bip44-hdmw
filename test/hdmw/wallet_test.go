14f62925adbffe0d1a31200826e3b609beb86903
orpheus <ryanachacon@gmail.com>
2018-09-26 22:11:58 -0700

    initialize wallet

--- test/hdmw/wallet_test.go
+++ test/hdmw/wallet_test.go
@@ -34,3 +34,15 @@ func TestCreateBitcoinNode(t *testing.T) {
 	coin := spew.Sdump(c)
 	t.Log(coin)
 }
+
+func TestCreateAndInitWallet(t *testing.T) {
+	wallet := hdmw.CreateWalletWithPassword("")
+	wallet.Initialize([]uint32{hdmw.TypeBitcoin})
+
+	if len(wallet.Coins) != 1 {
+		t.Error("wallet.coins does not have an len of 1")
+	}
+
+	w := spew.Sdump(wallet)
+	t.Log(w)
+}
